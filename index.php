da08777e860b229afdbe2b5bc6b98adccae296ea
Vinay Abburi <vinay.abburi@gmail.com>
2014-06-11 12:03:32 -0500

    batch file

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require('vendor/autoload.php');
 define('AMQP_DEBUG', true);
 use PhpAmqpLib\Connection\AMQPConnection;
 use PhpAmqpLib\Message\AMQPMessage;
-$url = parse_url(getenv('RABBITMQ_BIGWIG_RX_URL')) or exit('unable to read env variable ');
+$url = parse_url(getenv('RABBITMQ_BIGWIG_TX_URL')) or exit('unable to read env variable to send ');
 var_dump($url);
 
 $conn = new AMQPConnection($url['host'], 5672, $url['user'], $url['pass'], substr($url['path'], 1)) or exit('unable to open AMQP Connection');
@@ -18,6 +18,16 @@ $ch->queue_bind($queue, $exchange);
 $msg_body = 'the body';
 $msg = new AMQPMessage($msg_body, array('content_type' => 'text/plain', 'delivery_mode' => 2));
 $ch->basic_publish($msg, $exchange) or exit('unable to publish message on rabbitmq');
+
+$url = parse_url(getenv('RABBITMQ_BIGWIG_RX_URL')) or exit('unable to read env variable to receive ');
+var_dump($url);
+
+$conn = new AMQPConnection($url['host'], 5672, $url['user'], $url['pass'], substr($url['path'], 1)) or exit('unable to open AMQP Connection');
+$ch = $conn->channel();
+
+$exchange = 'amq.direct';
+$queue = 'basic_get_queue';
+
 
 $retrived_msg = $ch->basic_get($queue) or exit('unable to retrieve message from rabbitmq');;
 echo "printing msg retrieved";
