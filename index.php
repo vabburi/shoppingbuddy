9aa0166364c113bb710cf96129505861cde3886c
Vinay Abburi <vinay.abburi@gmail.com>
2014-06-11 12:01:28 -0500

    batch file

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ use PhpAmqpLib\Message\AMQPMessage;
 $url = parse_url(getenv('RABBITMQ_BIGWIG_RX_URL')) or exit('unable to read env variable ');
 var_dump($url);
 
-$conn = new AMQPConnection($url['host'], 5672, $url['user'], $url['pass'], substr($url['path'], 1));
+$conn = new AMQPConnection($url['host'], 5672, $url['user'], $url['pass'], substr($url['path'], 1)) or exit('unable to open AMQP Connection');
 $ch = $conn->channel();
 
 $exchange = 'amq.direct';
@@ -17,9 +17,9 @@ $ch->queue_bind($queue, $exchange);
 
 $msg_body = 'the body';
 $msg = new AMQPMessage($msg_body, array('content_type' => 'text/plain', 'delivery_mode' => 2));
-$ch->basic_publish($msg, $exchange);
+$ch->basic_publish($msg, $exchange) or exit('unable to publish message on rabbitmq');
 
-$retrived_msg = $ch->basic_get($queue);
+$retrived_msg = $ch->basic_get($queue) or exit('unable to retrieve message from rabbitmq');;
 echo "printing msg retrieved";
 var_dump($retrived_msg->body);
 $ch->basic_ack($retrived_msg->delivery_info['delivery_tag']);
