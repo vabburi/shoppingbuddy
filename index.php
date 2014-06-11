24d85ddc11f2306f58949bde4445749c891e1aff
Vinay Abburi <vinay.abburi@gmail.com>
2014-06-11 11:50:21 -0500

    batch file

--- index.php
+++ index.php
@@ -1,41 +1,8 @@
-<?php
-echo 'came in';
-exit;
-try
-{
-echo 'before try block';
-exit;
-require('vendor/autoload.php');
-define('AMQP_DEBUG', true);
-use PhpAmqpLib\Connection\AMQPConnection;
-use PhpAmqpLib\Message\AMQPMessage;
-$url = parse_url(getenv('RABBITMQ_BIGWIG_RX_URL'));
-var_dump($url);
-exit;
-$conn = new AMQPConnection($url['host'], 5672, $url['user'], $url['pass'], substr($url['path'], 1)) or exit('unable to open AMQP Connection');
-$ch = $conn->channel();
-
-$exchange = 'amq.direct';
-$queue = 'basic_get_queue';
-$ch->queue_declare($queue, false, true, false, false);
-$ch->exchange_declare($exchange, 'direct', true, true, false);
-$ch->queue_bind($queue, $exchange);
-
-$msg_body = 'the body';
-$msg = new AMQPMessage($msg_body, array('content_type' => 'text/plain', 'delivery_mode' => 2));
-$ch->basic_publish($msg, $exchange);
-
-$retrived_msg = $ch->basic_get($queue);
-echo 'printing msg retrieved';
-var_dump($retrived_msg->body);
-$ch->basic_ack($retrived_msg->delivery_info['delivery_tag']);
-
-$ch->close();
-$conn->close();	
-}
-catch (Exception $e) {
-    echo 'Caught exception: ',  $e->getMessage(), "\n";
-}
-
-echo 'after try..catch block';
-?>
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+ </body>
+</html>
